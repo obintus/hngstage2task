9b3731e54d400d7c9e968b4e4c7bf267c7eedb8a
obintus <danielobintus@gmail.com>
2021-08-19 01:11:04 +0100

    corrections

---
+++ style.js
@@ -0,0 +1,8 @@
+function loaderFunction(){
+    var loader = document.getElementById("overlay");
+    loader.style.display = "none";
+
+    var scrolling = document.getElementById("body_cover");
+    scrolling.style.overflowY = "scroll";
+    
+}
